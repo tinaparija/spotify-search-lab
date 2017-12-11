6bf0e3d3a4245da2077e4b8b7ccb3ac1265f9743
tinaparija <tinaparija@gmail.com>
2017-12-10 16:14:23 -0800

    logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 // wait for DOM to load before running JS
 $(document).ready( function() {
 
-  // check to make sure JS is loaded
-  console.log('JS is loaded!');
 
-  // your code here
+$(".search_form").on("submit", function(e) {
+	event.preventDefault();
+	$("#results").html("");
+	let searchTerm = $(".search-input").val()
+	$.ajax({
+		method: 'GET',
+		url:"http://content.guardianapis.com/search?q="+searchTerm+"&api-key=a91e9908-b93d-4f28-a5b7-efbbd3730208",
+		success: onSuccess,
+		error: console.log("bleh")
+	});
+	});
 
-});
+function onSuccess(json){
+	for (let i=0; i < json.response.results.length; i++){
+	$("#results").append(`<a href="${json.response.results[i].webUrl}" target="_blank">${json.response.results[i].webTitle}</a>`);
+};
+};
+
+}); 
