106296eaafe570f4cd8ab0104e30b8b5553e8d03
tinaparija <tinaparija@gmail.com>
2017-12-11 17:45:08 -0800

    feedback update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
-// wait for DOM to load before running JS
 $(document).ready( function() {
 
 
 $(".search_form").on("submit", function(e) {
-	event.preventDefault();
+	e.preventDefault();
 	$("#results").html("");
 	let searchTerm = $(".search-input").val()
 	$.ajax({
 		method: 'GET',
-		url:"http://content.guardianapis.com/search?q="+searchTerm+"&api-key=a91e9908-b93d-4f28-a5b7-efbbd3730208",
+		url: (`http://content.guardianapis.com/search?q="${searchTerm}"&api-key=a91e9908-b93d-4f28-a5b7-efbbd3730208`),
 		success: onSuccess,
-		error: console.log("bleh")
+		error: function (){console.log("bleh")}
 	});
 	});
 
